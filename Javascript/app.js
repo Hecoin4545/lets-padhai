879e82474d83fbdbe1bfac3942ec6a5a50edfef9
shah het <92454514+Hecoin4545@users.noreply.github.com>
2023-11-11 11:07:15 +0530

    Final Home Page Commit

--- Javascript/app.js
+++ Javascript/app.js
@@ -102,3 +102,19 @@ document.addEventListener('mouseup', dragStop);
 carousel.addEventListener('scroll', infiniteScroll);
 wrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
 wrapper.addEventListener('mouseleave', autoPlay);
+
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slide active';
+}
+
+function prevSlide() {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].className = 'slide active';
+}
